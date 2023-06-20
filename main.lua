5246dd7f03604f0dbf4095742a106c6076a1947b
girvel <widauka@ya.ru>
2023-06-21 02:56:54 +0500

    Clean up notes

--- main.lua
+++ main.lua
@@ -1,31 +1,23 @@
--- Questions for the dev team --
---
--- 1. Code style guidelines
---   1.1. Policy about explicit braces with single-argument atomic call
---   1.2. Indentation
---   1.3. Case
---   1.4. (!) Line width
--- 2. Should I use OOP-style state+behaviour or functional-style stateless functions+arguments?
--- 3. (?) Version of lua and usage of JIT
--- 4. (?) Should I implement constructors/finalizers, input/output holders for the assembly line or is it a single 
--- object?
-
-
 -- Dev notes for myself --
 --
 -- -. Make mechanisms asynchronous
 -- 2. README + github
 -- -. Async waits
 -- 4. Cover with docstrings
--- 5. Will a bunch of coroutines+asleep+cycle eat CPU? Consider a delay? Run benchmarks?
+-- -. Will a bunch of coroutines+asleep+cycle eat CPU? Consider a delay? Run benchmarks?
 -- 6. Array with O(1) index shift
+-- 7. OS-based blocking sleep -> use it as delay in async.sleep for optimization purposes
+-- 8. Consider that coroutine.yield() in async.sleep potentially takes some time
 
 
 -- Implementation notes for the review --
 --
--- I chose asynchronous architecture, because I've assumed that the mechanisms are external remote devices and you 
+-- I chose asynchronous architecture, because I have assumed that the mechanisms are external remote devices and you 
 -- need to wait for them to finish execution. If they are just a code abstraction and a part of the same program, I 
 -- would have consider using threading.
+--
+-- Also there is no advanced async/await in pure Lua, so I wrote a minimalistic module `async` to implement some basic
+-- constructions.
 
 
 -- Only cosmetic external libraries
